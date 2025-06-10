6dffcd83fe49510624ce87ab95609a02efa6e22c
ShanmugaRaj <shanmugaraj4696@gmail.com>
2025-06-10 13:07:11 +0530

    Update index.js

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,16 +1,18 @@
 
-import { useEffect , useState } from "react"
+import { useEffect , useState  } from "react"
 import JobCard from "../JobCard"
 import Filter from "../Filter"
 import Header from "../Header" 
 import "./index.css"
 import { useSearchParams } from "react-router-dom"
 
-const Home  = () => { 
+const Home  = () => {  
     const [jobs ,setJobs] = useState([])  ; 
     const [searchParams , setSearchParams] = useSearchParams()
 
-    useEffect(() => {
+    useEffect(() => { 
+         
+
         fetch(process.env.REACT_APP_API_URL+'/api/v1/jobs?'+searchParams)
         .then(res => res.json()) 
         .then(res => setJobs(res.jobs))
